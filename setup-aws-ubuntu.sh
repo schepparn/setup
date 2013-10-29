53ee0752b3626ed56aa815b8d6b356ccf621f109
Anders Dalen <andersdalen@gmail.com>
2013-10-29 18:57:52 +0000

    Updated node version and included express in setup

--- setup-aws-ubuntu.sh
+++ setup-aws-ubuntu.sh
@@ -10,12 +10,16 @@ curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+nvm install v0.10.21
+nvm use v0.10.21
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
 npm install -g jshint
+
+# Install express to start webserver
+# http://expressjs.com/
+npm install -g express
 
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
